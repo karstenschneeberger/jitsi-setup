f591ce8601c30f9d73cd59e639e053d70d9ff1d0
Karsten Schneeberger <karsten@karstenschneeberger.de>
2025-07-13 10:24:09 +0200

    SHOW_JITSI_WATERMARK: false

--- jitsi-setup.sh
+++ jitsi-setup.sh
@@ -289,6 +289,56 @@ ln -sf /etc/nginx/sites-available/$(hostname -f).conf /etc/nginx/sites-enabled/$
 echo "Entferne NGINX-Logfiles..."
 rm -f /var/log/nginx/access.log /var/log/nginx/error.log
 
+echo "Überschreibe /usr/share/jitsi-meet/interface_config.js mit bereinigter Version..."
+
+cat > /usr/share/jitsi-meet/interface_config.js << 'EOF'
+var interfaceConfig = {
+    APP_NAME: 'Jitsi Meet',
+    AUDIO_LEVEL_PRIMARY_COLOR: 'rgba(255,255,255,0.4)',
+    AUDIO_LEVEL_SECONDARY_COLOR: 'rgba(255,255,255,0.2)',
+    AUTO_PIN_LATEST_SCREEN_SHARE: 'remote-only',
+    BRAND_WATERMARK_LINK: '',
+    CLOSE_PAGE_GUEST_HINT: false,
+    DEFAULT_BACKGROUND: '#040404',
+    DEFAULT_WELCOME_PAGE_LOGO_URL: 'images/watermark.svg',
+    DISABLE_DOMINANT_SPEAKER_INDICATOR: false,
+    DISABLE_JOIN_LEAVE_NOTIFICATIONS: false,
+    DISABLE_PRESENCE_STATUS: false,
+    DISABLE_TRANSCRIPTION_SUBTITLES: false,
+    DISABLE_VIDEO_BACKGROUND: false,
+    DISPLAY_WELCOME_FOOTER: true,
+    DISPLAY_WELCOME_PAGE_ADDITIONAL_CARD: false,
+    DISPLAY_WELCOME_PAGE_CONTENT: false,
+    DISPLAY_WELCOME_PAGE_TOOLBAR_ADDITIONAL_CONTENT: false,
+    ENABLE_DIAL_OUT: true,
+    FILM_STRIP_MAX_HEIGHT: 120,
+    GENERATE_ROOMNAMES_ON_WELCOME_PAGE: true,
+    HIDE_INVITE_MORE_HEADER: false,
+    JITSI_WATERMARK_LINK: 'https://jitsi.org',
+    LANG_DETECTION: true,
+    LOCAL_THUMBNAIL_RATIO: 16 / 9,
+    MAXIMUM_ZOOMING_COEFFICIENT: 1.3,
+    MOBILE_APP_PROMO: true,
+    OPTIMAL_BROWSERS: [ 'chrome', 'chromium', 'firefox', 'electron', 'safari', 'webkit' ],
+    POLICY_LOGO: null,
+    PROVIDER_NAME: 'Jitsi',
+    RECENT_LIST_ENABLED: true,
+    REMOTE_THUMBNAIL_RATIO: 1,
+    SETTINGS_SECTIONS: [ 'devices', 'language', 'moderator', 'profile', 'calendar', 'sounds', 'more' ],
+    SHOW_BRAND_WATERMARK: false,
+    SHOW_CHROME_EXTENSION_BANNER: false,
+    SHOW_JITSI_WATERMARK: false,
+    SHOW_POWERED_BY: false,
+    SHOW_PROMOTIONAL_CLOSE_PAGE: false,
+    SUPPORT_URL: 'https://community.jitsi.org/',
+    UNSUPPORTED_BROWSERS: [],
+    VERTICAL_FILMSTRIP: true,
+    VIDEO_LAYOUT_FIT: 'both',
+    VIDEO_QUALITY_LABEL_DISABLED: false,
+    makeJsonParserHappy: 'even if last key had a trailing comma'
+};
+EOF
+
 echo "Lade systemd und nginx neu..."
 systemctl daemon-reexec
 systemctl restart nginx
