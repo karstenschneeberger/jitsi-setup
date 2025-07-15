b006a041a4b2855bd76dd4f51a1aa18af1198929
Karsten Schneeberger <karsten@karstenschneeberger.de>
2025-07-15 17:10:36 +0200

    Änderungen an meet.karstenschneeberger.de-config.js und logging.properties

---
+++ conf/meet.karstenschneeberger.de-config.js
@@ -0,0 +1,79 @@
+var subdir = '<!--# echo var="subdir" default="" -->';
+var subdomain = '<!--# echo var="subdomain" default="" -->';
+
+if (subdomain) {
+    subdomain = subdomain.substr(0, subdomain.length - 1).split('.')
+        .join('_')
+        .toLowerCase() + '.';
+}
+
+if (subdir.startsWith('<!--')) {
+    subdir = '';
+}
+
+if (subdomain.startsWith('<!--')) {
+    subdomain = '';
+}
+
+var enableJaaS = false;
+
+var config = {
+    hosts: {
+        domain: 'meet.karstenschneeberger.de',
+        muc: 'conference.' + subdomain + 'meet.karstenschneeberger.de',
+    },
+
+    bosh: 'https://meet.karstenschneeberger.de/' + subdir + 'http-bind',
+    websocket: 'wss://meet.karstenschneeberger.de/' + subdir + 'xmpp-websocket',
+
+    bridgeChannel: {
+    },
+
+    testing: {
+    },
+
+    enableNoAudioDetection: true,
+    enableNoisyMicDetection: true,
+
+    resolution: 1080,
+    maxFullResolutionParticipants: 2,
+
+    constraints: {
+        video: {
+            height: {
+                ideal: 720,
+                max: 720,
+                min: 240,
+            },
+        },
+    },
+
+
+    channelLastN: 5,
+
+    hideEmailInSettings: true,
+    disableThirdPartyRequests: true,
+
+
+    p2p: {
+        enabled: false,
+        stunServers: [
+            { urls: 'stun:meet.karstenschneeberger.de:3478' },
+        ],
+    },
+
+    analytics: {
+        disabled: true,
+    },
+
+
+    doNotStoreRoom: true,
+
+};
+
+// Set the default values for JaaS customers
+if (enableJaaS) {
+    config.dialInNumbersUrl = 'https://conference-mapper.jitsi.net/v1/access/dids';
+    config.dialInConfCodeUrl = 'https://conference-mapper.jitsi.net/v1/access';
+    config.roomPasswordNumberOfDigits = 10; // skip re-adding it (do not remove comment)
+}
